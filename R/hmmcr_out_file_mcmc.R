35b6884b6cc322abefedef1707b99b4d49a1cca8
jonathanpw <33842796+jonathanpw@users.noreply.github.com>
2024-12-10 22:22:26 +0100

    Add files via upload

---
+++ R/hmmcr_out_file_mcmc.R
@@ -0,0 +1,43 @@
+# This script file produces trace plots and histograms of the mcmc output files
+
+trace_plot = function( chain, n_post, steps, burnin){
+
+	# Matrix row indices for the posterior sample to use for GFF computation
+	index_post = (steps - burnin - n_post + 1):(steps - burnin)
+
+	# ----------------------------------------------------------------------------
+	# Create mcmc trace plots and histograms
+	# ----------------------------------------------------------------------------
+
+	# Plot and save the mcmc trace plots and histograms.
+	par_mean = par_median = upper = lower = rep( NA, ncol(chain))
+	pdf('trace_plot.pdf')
+	par(mfrow=c(3, 2))
+	for(r in c(3:ncol(chain),1:2)){
+
+		plot( NULL, xlab=NA, ylab=NA, xlim=c(1,length(index_post)),                           ylim=range(chain[,r]) )
+
+		lines( chain[,r], type='l', col='black')
+
+		par_mean[r] = round( mean(chain[,r]), 4)
+		par_median[r] = round( median(chain[,r]), 4)
+		upper[r] = round( quantile( chain[,r], prob=.975), 4)
+		lower[r] = round( quantile( chain[,r], prob=.025), 4)
+
+		hist( chain[,r], ylab=NA, main=NA, freq=F,
+			    breaks=sqrt(nrow(chain)),
+		      xlab=paste0('Mean = ',toString(par_mean[r]),
+					             ' Median = ',toString(par_median[r])))
+		abline( v=upper[r], col='red', lwd=2, lty=2)
+		abline( v=lower[r], col='purple', lwd=2, lty=2)
+	}
+	dev.off()
+
+	# ----------------------------------------------------------------------------
+}
+
+
+
+
+
+
---
+++ src/hmmcr_bookkeeping.cpp
@@ -0,0 +1,137 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <iostream>
+
+#include "hmmcr_bookkeeping.h"
+
+
+Para::Para(const arma::vec& par_init,
+     const arma::vec& par_index,
+     const arma::vec& par_unique,
+     const arma::mat& Z,
+     const arma::mat& X,
+     const int nr_state)
+  :
+  par_index(par_index),
+  par_unique(par_unique),
+  nr_state(nr_state),
+  nr_col_Z(Z.n_cols),
+  nr_col_X(X.n_cols)
+{
+  par.zeros(par_init.n_elem);
+  init.ones(nr_state);
+  zeta.zeros(nr_state * (nr_state - 1), nr_col_Z);
+  beta.zeros(nr_state, nr_col_X);
+  theta.zeros(nr_state, 2);
+
+  setPara(par_init);
+}
+
+void Para::setPara(const arma::vec& new_par) {
+  if (new_par.n_elem != par.n_elem) {
+    throw std::invalid_argument("new_par has incorrect size.");
+  }
+
+  par = new_par;
+
+  // indexes
+  std::vector<size_t> k = {1, 0, 0, 0};
+  std::vector<size_t> l = {0, 0, 0, 0};
+
+  for (size_t i = 0; i < par_unique.size(); ++i) {
+    int par_index_i = par_index[i];
+    int par_unique_i = par_unique[i];
+
+    // get values or set to default
+    double value = (par_unique_i > 0) ? par(par_unique_i - 1) : 0.0;
+
+    switch (par_index_i) {
+
+    case 0: { // init
+      init(k[0]++) = exp(value);
+      break;
+    }
+    case 1: { // zeta
+      zeta(k[1], l[1]++) = value;
+      if (l[1] >= nr_col_Z) {
+        l[1] = 0;
+        k[1]++;
+      }
+      break;
+    }
+    case 2: { // beta
+      beta(k[2], l[2]++) = value;
+      if (l[2] >= nr_col_X) {
+        l[2] = 0;
+        k[2]++;
+      }
+      break;
+    }
+    case 3: { // theta
+      theta(k[3], l[3]++) = exp(value);
+      if (l[3] >= 2) {
+        l[3] = 0;
+        k[3]++;
+      }
+      break;
+    }
+    default:
+      throw std::runtime_error("Invalid value in par_index.");
+    }
+  }
+}
+
+arma::vec Para::getPara() const {
+  return par;
+}
+
+arma::vec Para::getInit() const {
+  return init / arma::sum(init);
+}
+
+arma::mat Para::getZeta() const {
+  return zeta;
+}
+
+arma::rowvec Para::getZeta(size_t i, size_t j) const {
+  if (i == j) {
+    throw std::invalid_argument("Diagonal elements are not part of zeta.");
+  }
+  if (i > nr_state || j > nr_state) {
+    throw std::out_of_range("State indices out of range.");
+  }
+
+  size_t index = (i - 1) * (nr_state - 1) + (j - 1) - (i <= j);
+
+  std::cout << index << std::endl;
+
+  if (index >= zeta.n_rows) {
+    throw std::out_of_range("Computed zeta index is out of range.");
+  }
+  return zeta.row(index);
+}
+
+arma::mat Para::getBeta() const {
+  return beta;
+}
+
+arma::rowvec Para::getBeta(size_t state) const {
+  if (state >= beta.n_rows) {
+    throw std::out_of_range("State index out of range in beta.");
+  }
+  return beta.row(state);
+}
+
+arma::mat Para::getTheta() const {
+  return theta;
+}
+
+arma::rowvec Para::getTheta(size_t state) const {
+  if (state >= theta.n_rows) {
+    throw std::out_of_range("State index out of range in theta.");
+  }
+  return theta.row(state);
+}
+
+
